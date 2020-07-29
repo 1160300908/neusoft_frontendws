628b31101aa379ba19defc443961d7105dc7dc42
hithexiao <1250593221@qq.com>
2020-07-29 08:10:00 +0800

    添加了user的增删改查

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,45 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+//引入路由管理的组件
+//首页区组件
+
+//引入部门的组件
+import DepartmetnMain  from "./../components/department/main.vue";
+import DepartmetnList  from "./../components/department/list.vue";
+import DepartmetnAdd  from "./../components/department/add.vue";
+import DepartmetnModify  from "./../components/department/modify.vue";
+import DepartmetnView  from "./../components/department/view.vue";
+
+import UserMain  from "./../components/user/main.vue";
+import UserList  from "./../components/user/list.vue";
+import UserAdd  from "./../components/user/add.vue";
+import UserModify  from "./../components/user/modify.vue";
+import UserView  from "./../components/user/view.vue";
+
+
+
+
+
 
 Vue.use(VueRouter)
 
   const routes = [
+	{path:"/department",name:"departmentmain", component:DepartmetnMain,children:[
+		{path:"list",name:"departmentlist",component:DepartmetnList},
+		{path:"add",name:"departmentadd",component:DepartmetnAdd},
+		{path:"modify/:no",name:"departmentmodify",component:DepartmetnModify},
+		{path:"view/:no",name:"departmentview",component:DepartmetnView},
+		{path:"", redirect: "list" }
+	]},
+	
+	{path:"/user",name:"usermain", component:UserMain,children:[
+		{path:"list",name:"userlist",component:UserList},
+		{path:"add",name:"useradd",component:UserAdd},
+		{path:"modify/:no",name:"usermodify",component:UserModify},
+		{path:"view/:no",name:"userview",component:UserView},
+		{path:"", redirect: "list" }
+	]}
  
 ]
 
